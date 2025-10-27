be6a49d2ef5bb844fb851017c40c378371c35695
Andri Priyadi <dripriyadi@gmail.com>
2025-10-27 17:20:17 +0700

    fixed do-while 6

--- do-while/do-while.6.js
+++ do-while/do-while.6.js
@@ -1,3 +1,9 @@
+const readline = require("readline/promises");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 let soal = ["5 + 3 = ?", "10 - 4 = ?", "7 * 2 = ?"];
 let jawaban = [8, 6, 14];
 let jawabanUser;
@@ -5,4 +11,24 @@ let lanjut;
 let nomorSoal = 0;
 let benar = 0;
 // Tulis kode do-while loop di sini untuk kuis
-console.log("Kuis selesai! Skor: " + benar + "/" + nomorSoal);
+async function main() {
+    do {
+        console.log(`Soal ${nomorSoal +1}: ${soal[nomorSoal]}`)
+        jawabanUser =parseInt( await rl.question('Jawaban kamu: '))
+        if(jawabanUser === jawaban[nomorSoal] ){
+            console.log('Benar!')
+            benar++
+        } else{
+            console.log(`salah! Jawaban yang benar: ${jawaban[nomorSoal]}`)
+        }
+        nomorSoal++
+        if(nomorSoal < soal.length){
+            const tanya = await rl.question('Ingin lanjut ke soal berikutnya? (y/n)' )
+            lanjut = (tanya ==="y")
+        } else lanjut = false
+
+    } while (lanjut)
+        rl.close()
+        console.log("Kuis selesai! Skor: " + benar + "/" + nomorSoal);
+}
+main()
